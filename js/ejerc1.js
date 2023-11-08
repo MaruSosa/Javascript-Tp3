799d667e56c517b6e5be1a1ab3b7af371187d5ca
mariana <marianass712@gmail.com>
2023-11-08 19:18:28 -0300

    se realizo el ejercicio1

---
+++ js/ejerc1.js
@@ -0,0 +1,12 @@
+/* Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista los doce nombres del arreglo.*/
+const meses=['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'];
+
+function mostrarMeses(mes){
+    document.write(`<h2>Lista de Meses ${mes}</h2>`);
+    document.write(`<ul>`);
+    for (let i = 0; i < meses.length; i++){
+        document.write(`<li>${meses[i]}</li>`);
+    }
+    document.write(`</ul>`);
+}
+mostrarMeses('');
