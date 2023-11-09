71ba703178f24c4a2040b4a55ceff4489c40a8f1
mariana <marianass712@gmail.com>
2023-11-09 16:39:00 -0300

    se corrigio el ejercicio5

--- js/ejerc5.js
+++ js/ejerc5.js
@@ -1,7 +1,18 @@
 /* Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.*/
 
-const cadena=prompt('Ingrese una cadena de texto');
+const cadena = prompt('Ingrese una cadena de texto');
+analizarCadena(cadena);
 
-function mayMin(){
-    0
+function analizarCadena(cadena) {
+    if (cadena === null) {
+        document.write("Operación cancelada por el usuario.");
+    } else if (cadena === "") {
+        document.write("Error: Debes ingresar una cadena de texto válida.");
+    } else if (cadena === cadena.toUpperCase()) {
+        document.write("La cadena está formada solo por mayúsculas.");
+    } else if (cadena === cadena.toLowerCase()) {
+       document.write("La cadena está formada solo por minúsculas.");
+    } else {
+        document.write("La cadena es una mezcla de mayúsculas y minúsculas.");
+    }
 }
